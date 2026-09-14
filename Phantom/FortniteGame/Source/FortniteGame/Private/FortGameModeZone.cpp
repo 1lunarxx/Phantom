9694fcf16cbe04d48f2f522bb3403e0e64c0ea0e
lunar <offahigh200@gmail.com>
2026-09-13 21:46:50 -0700

    Update FortGameModeZone.cpp

--- Phantom/FortniteGame/Source/FortniteGame/Private/FortGameModeZone.cpp
+++ Phantom/FortniteGame/Source/FortniteGame/Private/FortGameModeZone.cpp
@@ -24,7 +24,40 @@ void FortGameModeZone::FinishWorldInitialization(AFortGameModeZone* FortGameMode
 
 APawn* FortGameModeZone::SpawnDefaultPawnFor_Implementation(AFortGameModeZone* FortGameModeZone, AController* NewPlayer, AActor* StartSpot)
 {
-	return FortGameModeZone->SpawnDefaultPawnFor_Implementation(NewPlayer, StartSpot);
+	APawn* DefaultPawn = FortGameModeZone->SpawnDefaultPawnFor_Implementation(NewPlayer, StartSpot);
+
+	if (AFortPlayerController* FortPlayerController = Cast<AFortPlayerController>(NewPlayer))
+	{
+		if (FortPlayerController->QuickBars == NULL)
+			FortPlayerController->QuickBars = GWorld->SpawnActor<AFortQuickBars>(FVector(), FRotator(), AFortQuickBars::StaticClass(), FortPlayerController);
+
+		if (AFortInventory* WorldInventory = FortPlayerController->GetWorldInventory())
+		{
+			// stw doesnt want startingitems so im forced to do this!!!!
+
+			if (FortGameModeZone->StartingItems.Num() <= 0)
+			{
+				UFortBuildingItemDefinition* BuildingItemData_Wall = Utils::StaticFindObject<UFortBuildingItemDefinition>(TEXT("BuildingItemData_Wall"), ANY_PACKAGE);
+				UFortBuildingItemDefinition* BuildingItemData_Floor = Utils::StaticFindObject<UFortBuildingItemDefinition>(TEXT("BuildingItemData_Floor"), ANY_PACKAGE);
+				UFortBuildingItemDefinition* BuildingItemData_Stair_W = Utils::StaticFindObject<UFortBuildingItemDefinition>(TEXT("BuildingItemData_Stair_W"), ANY_PACKAGE);
+				UFortBuildingItemDefinition* BuildingItemData_RoofS = Utils::StaticFindObject<UFortBuildingItemDefinition>(TEXT("BuildingItemData_RoofS"), ANY_PACKAGE);
+				UFortEditToolItemDefinition* EditToolItemDefinition = Utils::StaticFindObject<UFortEditToolItemDefinition>(TEXT("EditTool"), ANY_PACKAGE);
+
+				WorldInventory->AddItem(BuildingItemData_Wall, 1);
+				WorldInventory->AddItem(BuildingItemData_Floor, 1);
+				WorldInventory->AddItem(BuildingItemData_Stair_W, 1);
+				WorldInventory->AddItem(BuildingItemData_RoofS, 1);
+				WorldInventory->AddItem(EditToolItemDefinition, 1);
+			}
+
+			UAthenaPickaxeItemDefinition* AthenaPickaxeItemDefinition = Utils::StaticFindObject<UAthenaPickaxeItemDefinition>(TEXT("DefaultPickaxe"), ANY_PACKAGE);
+
+			if (AthenaPickaxeItemDefinition != NULL)
+				WorldInventory->AddItem(AthenaPickaxeItemDefinition->WeaponDefinition, 1);
+		}
+	}
+
+	return DefaultPawn;
 }
 
 void FortGameModeZone::Setup()
